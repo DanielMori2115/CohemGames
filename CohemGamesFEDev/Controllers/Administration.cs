4173fd3499166f7ea9589866caa1d5f5f8b9cfd9
DanielMori2115 <86133295+DanielMori2115@users.noreply.github.com>
2025-01-16 03:35:16 -0500

    Inclusion de template para vistas de administracion

---
+++ CohemGamesFEDev/Controllers/Administration.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CohemGamesFEDev.Controllers
+{
+    public class Administration : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
